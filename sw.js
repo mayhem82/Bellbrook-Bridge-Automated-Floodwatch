bddff620519c5f256c6092786ab6dadc5f9fd18d
mayhem82 <minkorecovery@gmail.com>
2026-09-19 07:56:24 +1000

    Add Floodwatch service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="bellbrook-floodwatch-v1";
+const CORE=["./","./index.html","./about.html","./icon.svg","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
