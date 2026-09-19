046f4e2945063df2a42a6103529b47ee585030f6
mayhem82 <minkorecovery@gmail.com>
2026-09-19 16:04:56 +1000

    Refresh PWA cache for Samsung Android fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="bellbrook-floodwatch-v1";
-const CORE=["./","./index.html","./about.html","./icon.svg","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE="bellbrook-floodwatch-v2";
+const CORE=["./","./index.html","./about.html","./icon-192.png","./icon-512.png","./manifest.webmanifest"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
