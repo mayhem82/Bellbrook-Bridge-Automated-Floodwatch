4e616c8feafe04d949248c12f3bf958f30858be3
mayhem82 <minkorecovery@gmail.com>
2026-09-22 07:49:37 +1000

    Deploy camera archive URL fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "bellbrook-floodwatch-";
-const CACHE = CACHE_PREFIX + "v5-2026.09.22.12";
+const CACHE = CACHE_PREFIX + "v6-2026.09.22.13";
 const CORE = ["./", "./index.html", "./about.html", "./icon-192.png", "./icon-512.png", "./manifest.webmanifest"];
 
 async function fetchWithDeadline(request, timeoutMs = 6000) {
