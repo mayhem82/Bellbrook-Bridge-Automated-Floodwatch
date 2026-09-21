05f848ff61816b1dda0c05a6388e3208ff539a13
mayhem82 <minkorecovery@gmail.com>
2026-09-22 07:55:19 +1000

    Deploy restored timelapse loader

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "bellbrook-floodwatch-";
-const CACHE = CACHE_PREFIX + "v7-2026.09.22.14";
+const CACHE = CACHE_PREFIX + "v8-2026.09.22.15";
 const CORE = ["./", "./index.html", "./about.html", "./icon-192.png", "./icon-512.png", "./manifest.webmanifest"];
 
 async function fetchWithDeadline(request, timeoutMs = 6000) {
